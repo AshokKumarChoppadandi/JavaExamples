b4f73595b68a42e3ef26b3b3c7faa781c3551d1d
Ashok Kumar Choppadandi <ashokkumar98778@gmail.com>
2024-11-14 20:18:05 +0530

    Basic Java code added to publish artifact to Maven Central repository

---
+++ src/main/java/io/github/ashokkumarchoppadandi/BasicCalculator.java
@@ -0,0 +1,21 @@
+package io.github.ashokkumarchoppadandi;
+
+/**
+ * A simple calculator class to perform basic arithmetic operations with two numbers
+ */
+public class BasicCalculator {
+    /**
+     * Default Constructor
+     */
+    public BasicCalculator() {}
+
+    /**
+     * A simple `add` method to add two integer numbers
+     * @param a Integer - addition argument1
+     * @param b Integer - addition argument1
+     * @return Integer - The addition of two input numbers
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/io/github/ashokkumarchoppadandi/BasicCalculatorTest.java
@@ -0,0 +1,27 @@
+package io.github.ashokkumarchoppadandi;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class BasicCalculatorTest {
+
+    public static BasicCalculator basicCalculator;
+
+    @BeforeAll
+    public static void initialBasicCalculatorObject() {
+        basicCalculator = new BasicCalculator();
+        System.out.println("Initialized Basic Calculator Object.");
+    }
+    @Test
+    public void testAdditionOfTwoInteger() {
+        int actual = basicCalculator.add(10, 20);
+        Assertions.assertEquals(actual, Math.addExact(10, 20));
+    }
+
+    @AfterAll
+    public static void exitUnitTests() {
+        System.out.println("All unit tests execution completed.");
+    }
+}
